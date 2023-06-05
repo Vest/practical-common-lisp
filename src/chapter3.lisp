6b9e0effc9c4b3f7fda0970176f9789b77a96767
Vest <Vest@users.noreply.github.com>
2023-06-05 21:37:38 +0200

    Added DB saving.

--- src/chapter3.lisp
+++ src/chapter3.lisp
@@ -28,6 +28,13 @@
     (loop (add-record (prompt-for-cd))
           (if (not (y-or-n-p "Another? [y/n]")) (return))))
 
+    (defun save-db (filename)
+      (with-open-file (out filename
+                           :direction :output
+                           :if-exists :supersede)
+        (with-standard-io-syntax
+          (print *db* out))))
+
     (add-record (make-cd "Roses" "Kathy Mattea" 7 t))
     (add-record (make-cd "Fly" "Dixie Chicks" 8 t))
     (add-record (make-cd "Home" "Dixie Chicks" 9 t))
